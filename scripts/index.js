875b0a9e3d2c90f83de297e775091ac0a529c802
Artem Pasko <guramibox777@gmail.com>
2024-04-12 18:07:25 +0300

    change content font

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ btn.addEventListener("click", generatePDF);
 function generatePDF() { 
     const element = document.getElementById('page'); 
     const opt = {
+        margin:       0.2,
         filename:     'pasko_cv.pdf',
         image:        { type: 'jpeg', quality: 0.98 },
         html2canvas:  { windowWidth: 1240, scale: 2 },
